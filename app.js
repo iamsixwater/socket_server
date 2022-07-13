a50768457b4c0ba555d04f8f76120c57aeb69a62
iamsixwater <soup.sixwater@gmail.com>
2022-07-14 08:17:35 +0900

    서버-클라이언트 간 텍스트 통신

---
+++ app.js
@@ -0,0 +1,65 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var fs = require('fs');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// socket.io setup
+app.io = require('socket.io')();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use(express.static('public'));
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+// socket.io handler
+// ws://localhost:8080/
+app.io.on('connection', (socket) => {
+  console.log('user connected');
+
+  socket.on('play', playMsg => {
+    
+
+
+    app.io.emit('play', playMsg); //여기로 오디오 들어가야함.
+    console.log(playMsg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+
+module.exports = app;
